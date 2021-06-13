b58ac8e87efbccf0d6fd8b7c816046bfad2f2dea
arielwc <arielwalkercochrane@gmail.com>
2021-06-13 16:33:13 +1200

    adds whakatauki file and background CSS

---
+++ whakatauki.js
@@ -0,0 +1,319 @@
+const whakatauki = [
+  {
+    maori: "Whāia te iti kahurangi ki te tūohu koe me he maunga teitei",
+
+    english:
+      "Seek the treasure you value most dearly: if you bow your head, let it be to a lofty mountain",
+
+    interpretation:
+      "This whakatauki is about aiming high or for what is truly valuable, but it's real message is to be persistent and don't let obstacles stop you from reaching your goal",
+  },
+
+  {
+    maori: "Whatungarongaro te tangata toitū te whenua",
+
+    english: "As man disappears from sight, the land remains",
+
+    intepretation:
+      "This demonstrates the holistic values of the Maori, and the utmost respect of Papatuanuku, the mother of the earth",
+  },
+
+  {
+    maori: "He tangata takahi manuhiri, he marae puehu",
+
+    english:
+      "A person who mistreats his guest has a dusty Marae (Meeting house)",
+
+    intepretation:
+      "Someone who disregards his visitors will soon find he has no visitors at all. This accentuates the importance of Manaakitanga, or hospitality with Maori society and culture",
+  },
+
+  {
+    maori: "Tangata ako ana i te whare, te turanga ki te marae, tau ana",
+
+    english:
+      "A person who is taught at home, will stand collected on the Marae (meeting house grounds)",
+
+    intepretation:
+      "A child who is given proper values at home and cherished within his family, will not only behave well amongst the family but also within society and throughout his life",
+  },
+
+  {
+    maori: "Te anga karaka, te anga koura, kei kitea te Marae",
+
+    english:
+      "The shells of the karaka berry, and the shells of the crayfish, should not be seen from the Marae",
+
+    intepretation:
+      "Although this clearly has a hygienic undertone, it also refers to discipline. A tribe or war party who disregards organization and has no concern for where they leave their rubbish and gear reflects poor leadership and discipline, thus becoming easy prey for a more regimented force",
+  },
+
+  {
+    maori: "Ka mate te kāinga tahi, ka ora te kāinga rua",
+
+    english: "When one house dies, a second lives",
+
+    intepretation:
+      "Historically used when two houses or families are merged due to the unfortunate circumstances of one particular family. However this could be used when something good emerges from misfortune",
+  },
+
+  {
+    maori: "Ka pū te ruha, ka hao te rangatahi",
+
+    english: "As a old net withers another is remade",
+
+    intepretation:
+      "When an elder is no longer fit to lead, a healthier leader will stand in his place",
+  },
+
+  {
+    maori: "Te amorangi ki mua, te hapai o ki muri",
+
+    english: "The leader at the front and the workers behind the scenes",
+
+    intepretation:
+      "This is a reference to Marae protocol where the speakers are at the front of the meeting house and the workers are at the back making sure everything is prepared and that the guests are well looked after. It is important to note that both jobs are equally important, and are like the ying and yang, for without one, everything would fail",
+  },
+
+  {
+    maori: "He kai kei aku ringa",
+
+    english: "There is food at the end of my hands",
+
+    intepretation:
+      "Said by a person who can use his basic abilities and resources to create success",
+  },
+
+  {
+    maori: "Kaua e mate wheke mate ururoa",
+
+    english: "Don't die like a octopus, die like a hammerhead shark",
+
+    intepretation:
+      "Octopus are renown for their lack of resistance when being captured, however a hammerhead shark will fight bitterly to the end, to the point that when you fillet it fresh, its meat quivers. Commonly used to encourage someone not to give up, no matter how hard the struggle is",
+  },
+
+  {
+    maori: "He mahi te ataa noho, e kii ana te wheke",
+
+    english: "It is the octopus who says sitting is working",
+
+    intepretation: "This proverb can be used for lazy person",
+  },
+
+  {
+    maori: "Matu nga ringa atu nga ringa raupo",
+
+    english: "Marry a man with calloused hands",
+
+    intepretation:
+      "Calloused hands are earned through hard work. This proverb suggests to woman to find a man who has an excellent work ethic",
+  },
+
+  {
+    maori: "Tama tu tama ora, tama noho tama mate",
+
+    english:
+      "An active person will remain healthy while a lazy one will become sick",
+
+    intepretation:
+      "An word of encouragement to urge children to participate in activities and exercise. It is like saying, To stand is to live, to lie down is to die.",
+  },
+
+  {
+    maori: "Ma whero ma pango ka oti ai te mahi",
+
+    english: "With red and black the work will be complete",
+
+    intepretation:
+      "This refers to co-operation where if everyone does their part, the work will be complete. The colours refer to to the traditional kowhaiwhai patterns on the inside of the meeting houses",
+  },
+
+  {
+    maori: "Naku te rourou nau te rourou ka ora ai te iwi",
+
+    english: "With your basket and my basket the people will live",
+
+    intepretation:
+      "Again referring to co-operation and the combination of resources to get ahead. This proverb can be very useful and is often said",
+  },
+
+  {
+    maori: "He waka eke noa",
+
+    english: "A canoe which we are all in with no exception",
+
+    intepretation:
+      "We are all in this together. An example of when this can be used perhaps when a group of you are going to the movies but one of them doesn't have any money so wouldn't be able to go along. You can say he waka eke noa, meaning you will pay as you are all in one group and it would not be the same if they were to miss out",
+  },
+
+  {
+    maori: "Ehara taku toa, he takitahi, he toa takitini",
+
+    english:
+      "My success should not be bestowed onto me alone, as it was not individual success but success of a collective",
+
+    intepretation: "Said humbly when acknowledged",
+  },
+
+  {
+    maori: "Kāore te kumara e kōrero mō tōna ake reka",
+
+    english: "The kumara (sweet potato) does not say how sweet he is",
+
+    intepretation: "This proverbs accentuates the the value of humbleness",
+  },
+
+  {
+    maori: "Waiho ma te tangata e mihi",
+
+    english: "Leave your praises for someone else",
+
+    intepretation: "Again referring to humbleness",
+  },
+
+  {
+    maori: "E kore te patiki e hoki ki tona puehu",
+
+    english: "The flounder (fish) does not return to his dust",
+
+    intepretation: "Do not make the same mistake twice",
+  },
+
+  {
+    maori: "Ahakoa he iti he pounamu",
+
+    english: "Although it is small, it is greenstone",
+
+    intepretation:
+      "This is a humble way to deliver a small gift. Greenstone (jade) is an extremely useful commodity which is considered very precious, so although you may not be presenting greenstone, the word pounamu stands as a metaphor for something precious or a treasure from the heart",
+  },
+
+  {
+    maori: "E hoa ma, ina te ora o te tangata",
+
+    english: "My friends, this is the essence of life",
+
+    intepretation:
+      "This proverb is an exclamation which can be used when someone is surprised or satisfied. Generally used at the dinner table from a guest who is appreciative of the meal he has received",
+  },
+
+  {
+    maori: "He kotuku rerenga tahi",
+
+    english: "A white heron flies once",
+
+    intepretation:
+      "This is used on an occasion when something very special and unusual takes place",
+  },
+
+  {
+    maori: "Kua hinga te totara i te wao nui a Tane",
+
+    english: "The totara has fallen in the forest of Tane",
+
+    intepretation:
+      "A totara is a huge tree that grows for hundreds of years. For one of them to fall is a great tragedy. This proverb is said when someone of importance passes away. The Totara is a native tree of New Zealand",
+  },
+
+  {
+    maori: "Kua takoto te manuka",
+
+    english: "The leaves of the manuka tree have been laid down",
+
+    intepretation:
+      "This is a form of wero, that is preformed in very formal situations on the Marae. It is when you are challenged and you answer that challenge depending on how pick up the leaves. The wero is to see whether you come in peace or as an enemy. This proverb is used when being challenged, or you have a challenge ahead of you",
+  },
+
+  {
+    maori: "Ta te tamariki tana mahi wawahi tahā",
+
+    english: "It is the job of the children to smash the calabash",
+
+    intepretation:
+      "This proverb is similar to, boys will be boys. The calabash was a valuable tool for the transportation of food and water and was also used to heat water. A child who has clumsy and of a playful nature has no idea of the importance of this tool and through neglect may accidentally break it. This is not the fault of the child and they should not be punished for what is their nature. Here the calabash is a metaphor for rules and regulations, which from time to time children and adolescents may over step in order for them to develop themselves",
+  },
+
+  {
+    maori: "Ko taku reo taku ohooho, ko taku reo taku mapihi mauria",
+
+    english:
+      "My language is my awakening, my language is the window to my soul",
+
+    intepretation:
+      "This is a proverb closely associated with language revitalization, a struggle which is very important in maintaining culture",
+  },
+
+  {
+    maori: "Ki te kahore he whakakitenga ka ngaro te iwi",
+
+    english: "Without foresight or vision the people will be lost",
+
+    intepretation:
+      "Said by Kingi Tawhiao Potatau te Wherwhero, to show the urgency of unification and strong Maori leadership",
+  },
+
+  {
+    maori: "He kākano ahau i ruia mai i Rangiātea",
+
+    english: "I am a seed which was sewn in the heavens of Rangiatea",
+
+    intepretation:
+      "A famous proverb from the Aotea waka, which shows the important of your genealogy and your culture",
+  },
+
+  {
+    maori: "He tangata ki tahi",
+
+    english: "A man who speaks once",
+
+    intepretation:
+      "There are two interpretations of this proverb. A person man who speaks once, referring to an authoritive figure who acquires instant response. The other is a person who is a man of his word, says something and sticks to it",
+  },
+
+  {
+    maori: "He patu te ua ki runga, he ngutu wāhine ki raro",
+
+    english:
+      "Like the rain that pelts down upon the roof, the lips of women move below",
+
+    intepretation:
+      "This is used to compare the way the rain falls with the way women gossip. The sound of the clatter of the rain on the roof is similar to the chatter of the women",
+  },
+
+  {
+    maori: "He ihu kurī, he tangata haere",
+
+    english: "Like a dog follows his nose, man will find opportunity",
+
+    intepretation:
+      "Used to refer to a unjust actions of criminals or lowlife. Comparing a man to a dog. Negative connotation",
+  },
+
+  {
+    maori: "Hōhonu kaki, pāpaku nana",
+
+    english: "A deep neck, but a shallow outcome",
+
+    intepretation:
+      "Used to refer to a lazy worker, who has a healthy appetite but offers very little help",
+  },
+
+  {
+    maori: "He manako te koura i kore ai",
+
+    english: "There are no crayfish as you set your heart on them",
+
+    intepretation:
+      "This is similar to not putting all your eggs in one basket. You could also consider this as Don't count your chickens before they are hatched",
+  },
+
+  {
+    maori: "He maroro kokati ihu waka",
+
+    english: "The flying fish which jumps across the nose of the canoe",
+
+    intepretation:
+      "Here the Maroro or flying fish is a metaphor for someone who crossed a war party and is killed to ward of bad luck. If something bad happens in a superstitious way, you say this to acknowledge that something is a fault. It's a bad omen that someone bumped into you when you're on the way to war so you have to kill them",
+  },
+];
